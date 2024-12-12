ec2e03db34ba65260d469940b778adc4d56f18e9
Prashant <chauhanprashant480@gmail.com>
2024-12-12 12:50:38 +0530

    book and author models defined

--- index.js
+++ index.js
@@ -7,22 +7,60 @@ const dbPassword = process.env.DATABASE_PASSWORD;
 const dbHost = process.env.DATABASE_HOST;
 const dbPort = process.env.DATABASE_PORT;
 
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 pgURL = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
 // console.log(pgURL);
 
-const pgConnection = new Sequelize(
+const sequelize = new Sequelize(
   `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`
 );
 
 try {
-  pgConnection.authenticate();
+  sequelize.authenticate();
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
   return;
 }
+
+const Book = sequelize.define(
+  "Book",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    published_date: {
+      type: DataTypes.TIME,
+    },
+  },
+  {}
+);
+
+const Author = sequelize.define(
+  "Author",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    biography: {
+      type: DataTypes.STRING,
+    },
+    born_date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {}
+);
+
+console.log(Book === sequelize.models.Book);
+console.log(Author === sequelize.models.Author);
+sequelize.sync({ force: false });
 
 const express = require("express");
 const app = express();
@@ -35,3 +73,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`server started at port ${port}`);
 });
+
+// sequelize.close();
