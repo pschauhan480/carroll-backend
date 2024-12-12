79e63664d9bee2f75bdadb581d1b3a1a2846557f
Prashant <chauhanprashant480@gmail.com>
2024-12-12 12:36:34 +0530

    sequelize and postgres connection established with secrets using dotenv

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbUser = process.env.DATABASE_USER;
+const dbName = process.env.DATABASE_NAME;
+const dbPassword = process.env.DATABASE_PASSWORD;
+const dbHost = process.env.DATABASE_HOST;
+const dbPort = process.env.DATABASE_PORT;
+
+const { Sequelize } = require("sequelize");
+
+pgURL = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+// console.log(pgURL);
+
+const pgConnection = new Sequelize(
+  `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`
+);
+
+try {
+  pgConnection.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+  return;
+}
+
 const express = require("express");
 const app = express();
 const port = 3000;
