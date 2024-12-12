2a5a86e1b6390212c219a5f54f26f0f50d2a9749
Prashant <chauhanprashant480@gmail.com>
2024-12-13 00:56:02 +0530

    create and query books mutation and query updated with parameters

--- schema.js
+++ schema.js
@@ -3,34 +3,50 @@ import { GraphQLScalarType, Kind } from "graphql";
 import { Book, Author } from "./pg_operations.js";
 
 export const typeDefs = `#graphql
-  scalar Date
+    scalar Date
 
-  type Book {
-    title: String
-    description: String
-    published_date: Date
-  }
+    type Book {
+        title: String
+        description: String
+        published_date: Date
+    }
 
-  type Author {
-    name: String
-    biography: String
-    born_date: Date
-  }
+    type Author {
+        name: String
+        biography: String
+        born_date: Date
+    }
 
-  type Query {
-    books: [Book]
-    authors: [Author]
-  }
+    type Query {
+        books: [Book]
+        authors: [Author]
+    }
+
+    input BookInput {
+        title: String
+        description: String
+        published_date: Date
+    }
+
+    type Mutation {
+        createBook(book: BookInput): Book
+    }
 `;
 
 export const resolvers = {
     Query: {
-        books: () => {
+        books: async (_, req) => {
             if (Book) {
                 return Book.findAll();
             } else {
                 return [];
             }
+        },
+    },
+    Mutation: {
+        createBook: async (_, req) => {
+            console.log("create book request", req.book);
+            return {};
         },
     },
     Date: new GraphQLScalarType({
