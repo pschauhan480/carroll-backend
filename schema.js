4cb59dac0a6bf87d2b60c83b1a0e5c9e7d461dd4
Prashant <chauhanprashant480@gmail.com>
2024-12-13 12:45:51 +0530

    book fetch by author id field enabled

--- schema.js
+++ schema.js
@@ -29,7 +29,7 @@ export const typeDefs = `#graphql
 
     type Query {
         books: [Book]
-        book(id: ID, title: String): [Book]
+        book(id: ID, title: String, authorid: ID): [Book]
         authors: [Author]
         author(id: ID, name: String): [Author]
         bookreviews(bookid: ID!): [BookReview]
